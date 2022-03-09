db0eb9d069b47f5fea40862947aec4906b337033
Luke Marsden <me@lukemarsden.net>
2022-03-09 13:15:37 +0000

    bump sameproject to version with fix for imports

--- 03-road-signs/build-and-push.sh
+++ 03-road-signs/build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
-export IMAGE=combinatorml/jupyterlab-tensorflow-opencv:0.6
+export IMAGE=combinatorml/jupyterlab-tensorflow-opencv:0.7
 docker build -t $IMAGE .
 docker push $IMAGE
