47c1cb786298c37578ba6c58511a8b0ab36b114c
Luke Marsden <me@lukemarsden.net>
2022-03-09 16:55:46 +0000

    bump same version in notebook image

--- 03-road-signs/build-and-push.sh
+++ 03-road-signs/build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
-export IMAGE=combinatorml/jupyterlab-tensorflow-opencv:0.7
+export IMAGE=combinatorml/jupyterlab-tensorflow-opencv:0.8
 docker build -t $IMAGE .
 docker push $IMAGE
